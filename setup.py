925a9d1d1735a160bac2e7948e618e1481a68aec
Exseivier_Javier_Montalvo_Arredondo_UAAAN <exseivier@users.noreply.github.com>
2023-11-16 11:36:19 -0600

    Setup.py was modified.
    Some issues were corrected

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     author_email='buitrejma@gmail.com',
     py_modules=['not_alike3.nal', 'not_alike3.utils'],
     packages=['not_alike3'],
-    python_requires='>=3.9',
+    python_requires='>=3.10',
     description='Command pipeline that finds not alike regions of query genome compared to at least one genome form a huge list of different genomes.',
     long_description = open('README.md', 'r').read(),
     install_requires =['click', 'pandas', 'biopython'],
@@ -46,7 +46,7 @@ setup(
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
         ],
         )
